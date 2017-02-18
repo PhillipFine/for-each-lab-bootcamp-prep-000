b734b1d30ffa9f6735cc11c4cd84d15d8016069c
Phillip Fine <phillipfine@yahoo.com>
2017-02-18 20:05:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,3 @@
+function iterativeLog(arr) {arr.forEach (function(cV,index,array) {console.log(`${index}: ${cV}`) } )  }
+function iterate(cB) {arr1=[1,2,3]; arr1.forEach(cB); return arr1}
+function doToArray(arr2,callBack) {arr2.forEach(callBack)}
